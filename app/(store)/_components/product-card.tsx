96bda255a4dcaaa21075d4517172e58be430ac1b
Faizan Asif <faizanasif.dev@gmail.com>
2024-04-17 15:08:10 +0200

    feat:add product page

--- app/(store)/_components/product-card.tsx
+++ app/(store)/_components/product-card.tsx
@@ -5,25 +5,33 @@ import { Image as ImageDb, Product } from '@prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
 
+type ProductCardProps = {
+  product: Product & {
+    images: ImageDb[]
+  }
+  isRelatedProduct?: boolean
+}
+
 const ProductCard = ({
   product,
-}: {
-  product: Product & { images: ImageDb[] }
-}) => {
+  isRelatedProduct = false,
+}: ProductCardProps) => {
   return (
     <article>
-      <Link href={`/products/${product.id}`} className="group space-y-3">
-        <div className="relative h-52 w-64 overflow-clip bg-stone-100 rounded-sm ">
+      <Link href={`/products/${product.id}`} className="group space-y-3 ">
+        <div className="relative min-h-52  overflow-clip bg-stone-100 rounded-sm ">
           <Image
             src={product.images.find(image => image.isPrimary)!.url}
             fill
             alt={product.title}
-            className="object-contain group-hover:scale-110 px-4"
+            className="object-contain group-hover:scale-110 p-4 aspect-square max-w-full"
           />
         </div>
-        <h4>{product.title}</h4>
-        <div>
-          <p className="text-secondary-foreground">{product.description}</p>
+        <h4 className={isRelatedProduct ? 'text-sm' : ''}>{product.title}</h4>
+        <div className={isRelatedProduct ? 'text-sm' : ''}>
+          <p className="text-secondary-foreground truncate">
+            {product.description}
+          </p>
           <span>{formatter.format(Number(product.price))}</span>
         </div>
       </Link>
--- app/(store)/products/[productId]/page.tsx
+++ app/(store)/products/[productId]/page.tsx
@@ -35,35 +35,39 @@ const ProductsPage = async ({
   return (
     <section className="mt-8">
       <div className="container space-y-24">
-        <article className="grid grid-cols-[auto_1fr] gap-20 ">
-          <div className="relative h-64 w-96 bg-stone-100 rounded-sm">
+        <article className="grid grid-cols-[1fr_1fr] gap-20 ">
+          <div className="relative bg-stone-100 rounded-sm min-h-[400px]">
             <Image
               src={product.images[0].url}
               fill
               alt={product.title}
-              className="object-contain p-8 "
+              className="object-contain p-6 aspect-square max-w-full "
             />
           </div>
-          <div className="py-4 border-red-400 border-2 grid ">
-            <div className="space-y-1 ">
+          <div className="py-4 flex flex-col ">
+            <div className="space-y-2 ">
               <h4 className="font-bold">{product.title}</h4>
               <small className="inline-block">{product.category.title}</small>
               <p className="text-sm">
                 {formatter.format(Number(product.price))}
               </p>
             </div>
-            <div className="mt-6 mb-4">
+            <div className="mt-8 mb-4 space-y-1 flex-1">
               <h6>Description</h6>
               <p className="text-secondary-foreground">{product.description}</p>
             </div>
-            <Button className="self-end">Add To Cart</Button>
+            <Button className="w-full">Add To Cart</Button>
           </div>
         </article>
         <div>
           <h5>Related Products</h5>
-          <div className="flex flex-wrap gap-4">
+          <div className="grid grid-cols-3 gap-4 mt-8">
             {relatedProducts.map(product => (
-              <ProductCard key={product.id} product={product} />
+              <ProductCard
+                key={product.id}
+                product={product}
+                isRelatedProduct
+              />
             ))}
           </div>
         </div>
