17bcc8547663630061cd0d817b20f4a029952b7c
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-06 15:36:32 +0200

    prettier tailwind

--- app/(store)/_components/auth-buttons.tsx
+++ app/(store)/_components/auth-buttons.tsx
@@ -1,14 +1,29 @@
-import { Button } from '@/components/ui/button'
-import { checkRole } from '@/lib/roles'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import { checkRole } from "@/lib/roles";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import Link from "next/link";
 
 const AuthButtons = () => {
   return (
     <>
       <SignedIn>
-        <Link href={'/orders'}>Orders</Link>
-        {checkRole('admin') ? <Link href={'/admin'}>Dashboard</Link> : null}
+        <Link
+          href={"/orders"}
+          className="px-2
+               shadow-[inset_0_0_0_0_hsl(var(--secondary))] transition-all duration-500 hover:text-primary-foreground hover:shadow-[inset_200px_0_0_0_hsl(var(--primary))] "
+        >
+          Orders
+        </Link>
+        {checkRole("admin") ? (
+          <Link
+            href={"/admin"}
+            className=" flex items-center gap-1 rounded-sm bg-secondary px-2"
+          >
+            <LayoutDashboard size={16} />
+            Dashboard
+          </Link>
+        ) : null}
         <UserButton />
       </SignedIn>
       <SignedOut>
@@ -17,7 +32,7 @@ const AuthButtons = () => {
         </SignInButton>
       </SignedOut>
     </>
-  )
-}
+  );
+};
 
-export default AuthButtons
+export default AuthButtons;
