3ba9c2ff2c9d331b20ac769a3d7b231cb9f94c91
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-08 16:51:58 +0200

    responsive overview dashboard

--- app/(admin)/admin/(Overview)/page.tsx
+++ app/(admin)/admin/(Overview)/page.tsx
@@ -7,25 +7,24 @@ import {
 import { formatter } from "@/lib/utils";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { CircleDollarSign, ShoppingBag } from "lucide-react";
+import { redirect } from "next/navigation";
 import DashboardDataCard from "./_components/dashboard-data-card";
 import OverviewChart from "./_components/overview-chart";
 
 const page = async () => {
-  auth().protect();
   const aggregations = await fetchDashboardAggregations();
   const user = await currentUser();
 
   const orderItemsCount = await countPaidOrderItems();
   const graphData = await fetchGraphData();
-  console.log(graphData);
 
-  if (!user) return;
+  if (!user) redirect("/");
 
   return (
-    <section className="  flex-1 space-y-6">
+    <section className="flex-1 space-y-6">
       <Heading title="Dashboard" description="Overview of your store" />
-      {/* <h1>Welcome back, {user?.firstName}</h1> */}
-      <div className="grid grid-cols-3 gap-4">
+      <h1>Welcome back, {user?.firstName}</h1>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <DashboardDataCard
           title="Total Revenue"
           data={formatter.format(
