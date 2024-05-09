5d7891a8389c6192fe15d6c096fdbf6869c3fe5a
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-09 14:10:38 +0200

    responsive hero

--- app/(store)/_components/category-card.tsx
+++ app/(store)/_components/category-card.tsx
@@ -1,16 +1,16 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
 
 type CategoryCardProps = {
-  title: string
-  imgUrl: string
-}
+  title: string;
+  imgUrl: string;
+};
 
 const CategoryCard = ({ title, imgUrl }: CategoryCardProps) => {
   return (
-    <Card className="relative min-h-[360px] bg-stone-200 flex items-end w-full">
+    <Card className="relative flex min-h-[360px] w-full items-end bg-secondary">
       <CardContent className="grid w-full">
         <Image src={imgUrl} alt={title} fill className="object-cover" />
         <Button asChild className="z-10">
@@ -18,6 +18,6 @@ const CategoryCard = ({ title, imgUrl }: CategoryCardProps) => {
         </Button>
       </CardContent>
     </Card>
-  )
-}
-export default CategoryCard
+  );
+};
+export default CategoryCard;
--- app/(store)/_components/hero.tsx
+++ app/(store)/_components/hero.tsx
@@ -1,26 +1,36 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import heroImage from "@/public/images/hero-1.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="container flex-1 ">
-      <div className="relative h-[704px] bg-stone-200 rounded-md grid grid-cols-2 items-center justify-items-end">
+    <section className="container">
+      <div className="relative flex aspect-[1499/704] max-h-[884px] max-w-[1560px] items-center rounded-md bg-secondary">
         <Image
-          src="/images/hero-1.png"
+          src={heroImage}
           fill
-          alt="hero"
-          className="object-cover"
+          quality={100}
+          placeholder="blur"
+          alt="image of two Iphones and two laptops "
+          className="object-cover object-center lg:object-contain"
+          priority
         />
-        <div className="ml-16 z-10">
-          <h1>Discover the latest tech innovations.</h1>
-          <p className="mt-2">Shop now and upgrade your digital experience</p>
+        <div className="z-10 w-3/5 py-8 pl-4 md:pl-8 xl:pl-16">
+          <h1 className=" text-xl md:text-4xl lg:text-5xl xl:text-6xl">
+            Discover the latest
+            <br />
+            tech innovations.
+          </h1>
+          <p className="mt-2  text-sm md:text-base lg:text-lg xl:text-xl">
+            Shop now and upgrade your digital experience
+          </p>
           <Button className="mt-6" asChild>
-            <Link href="/products">Shop now</Link>
+            <Link href="/products">SHOP NOW</Link>
           </Button>
         </div>
       </div>
     </section>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
