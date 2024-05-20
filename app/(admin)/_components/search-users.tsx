2f2a1b7b8a2c8293c27c7eb6e7c6bc0075be4f37
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-20 15:41:58 +0200

    settings page styling

--- app/(admin)/_components/search-users.tsx
+++ app/(admin)/_components/search-users.tsx
@@ -1,26 +1,32 @@
-'use client'
+"use client";
 
-import { usePathname, useRouter } from 'next/navigation'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { usePathname, useRouter } from "next/navigation";
 
 export const SearchUsers = () => {
-  const router = useRouter()
-  const pathname = usePathname()
+  const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <div>
       <form
-        onSubmit={async e => {
-          e.preventDefault()
-          const form = e.currentTarget
-          const formData = new FormData(form)
-          const queryTerm = formData.get('search') as string
-          router.push(pathname + '?search=' + queryTerm)
+        className="space-y-2"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const form = e.currentTarget;
+          const formData = new FormData(form);
+          const queryTerm = formData.get("search") as string;
+          router.push(pathname + "?search=" + queryTerm);
         }}
       >
-        <label htmlFor="search">Search for Users</label>
-        <input id="search" name="search" type="text" />
-        <button type="submit">Submit</button>
+        <Label htmlFor="search">Search for Users</Label>
+        <div className="flex items-center gap-2">
+          <Input id="search" name="search" type="text" />
+          <Button type="submit">Submit</Button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
