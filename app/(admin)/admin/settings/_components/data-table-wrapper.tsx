9c95eb48abd783f6aabb3a80789be12e5f6e31c3
Faizan Asif <faizanasif.dev@gmail.com>
2024-06-10 11:54:29 +0200

    optimistic  settings table

---
+++ app/(admin)/admin/settings/_components/data-table-wrapper.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { DataTable } from "@/components/ui/data-table";
+import { deleteCategory, deleteProduct } from "@/lib/actions";
+import { ProductPageInfo } from "@/lib/definitions";
+import { Category, Image } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useOptimistic } from "react";
+import { toast } from "sonner";
+import { getCategoryColumns } from "./columns";
+
+const DataTableWrapper = ({
+  categories,
+}: {
+  categories: (Category & { images: Image[] })[];
+}) => {
+  const [optimisticCategories, setOptimisticCategories] = useOptimistic(
+    categories,
+    (state, optimisticValue) => {
+      return state.filter((item) => item.id !== optimisticValue);
+    },
+  );
+  const router = useRouter();
+
+  async function handleDeleteProduct(categoryId: string) {
+    setOptimisticCategories(categoryId);
+    await deleteCategory(categoryId);
+    router.refresh();
+    toast.success("Category deleted");
+  }
+  const columns = getCategoryColumns(handleDeleteProduct);
+
+  return (
+    <DataTable
+      columns={columns}
+      data={optimisticCategories}
+      placeHolder="categories"
+    />
+  );
+};
+
+export default DataTableWrapper;
